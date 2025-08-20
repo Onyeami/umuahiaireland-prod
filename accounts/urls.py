eabaf946de381725940dd5ab0c3d77953b65e914
Ifeanyichukwu Onyekwelu <ifeanyionyekwelu786@gmail.com>
2025-08-20 14:38:45 +0100

    Fixing the password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import (
     user_login,
     user_register,
     user_logout,
     verificaton_email_sent,
     verify_account,
-    resend_verification_email
+    resend_verification_email,
+    CustomPasswordResetView,
 )
 
 app_name = "accounts"
@@ -25,4 +27,21 @@ urlpatterns = [
     ),
     path("verification/email/<str:token>/", verify_account, name="verify_account"),
     path("logout/", user_logout, name="logout"),
+    # Password Reset URLs with HTML support
+    path("password_reset/", CustomPasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
